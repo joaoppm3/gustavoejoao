e9939365424944628f3336e9a9cb9f5f28ef67a3
João Pedro <joaoppm3@al.insper.edu.br>
2025-05-05 22:30:19 -0300

    questao 3

--- funcoes.py
+++ funcoes.py
@@ -16,3 +16,11 @@ def guardar_dado(dados_rolados, dados_guardados, indice):
     return [dados_rolados, dados_guardados]
 
 
+
+def remover_dado(dados_rolados, dados_guardados, indice):
+    dado = dados_guardados[indice]
+    del dados_guardados[indice]
+    dados_rolados.append(dado)
+    return [dados_rolados, dados_guardados]
+
+
