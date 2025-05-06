d50671f3db6740ce56c77d66c34ec904c31e6d2d
João Pedro <joaoppm3@al.insper.edu.br>
2025-05-06 16:10:05 -0300

    questao 5

--- funcoes.py
+++ funcoes.py
@@ -16,7 +16,6 @@ def guardar_dado(dados_rolados, dados_guardados, indice):
     return [dados_rolados, dados_guardados]
 
 
-
 def remover_dado(dados_rolados, dados_guardados, indice):
     dado = dados_guardados[indice]
     del dados_guardados[indice]
@@ -28,4 +27,11 @@ def calcula_pontos_regra_simples(dados_rolados):
     for i in range(len(dados_rolados)):
         pontos_regra_simples[dados_rolados[i]] += dados_rolados[i]
     return pontos_regra_simples
+
+
+def calcula_pontos_soma(dados_rolados):
+    total = 0
+    for i in range(len(dados_rolados)):
+        total += dados_rolados[i]
+    return total
 
